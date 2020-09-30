f018bdeb089ddfd3a1e702c081f5f6cf39d3fc1f
Felipe Amador <felipedavidamador@gmail.com>
2020-09-30 16:43:51 -0300

    feat: add kits app

---
+++ kits/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class KitsConfig(AppConfig):
+    name = 'kits'
--- src/settings.py
+++ src/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
 
     # Apps.
     "accounts",
-    "products"
+    "products",
+    "kits"
 ]
 
 MIDDLEWARE = [
