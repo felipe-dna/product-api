db81f8e2ec323a60c4e10f35ff57861d906d2018
Felipe Amador <felipedavidamador@gmail.com>
2020-09-29 21:21:33 -0300

    feat: dd the rest framework configurations

--- src/settings.py
+++ src/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # rest framework.
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
@@ -120,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
