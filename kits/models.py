8b63e2fdd70b5c4765fb5a1eb4872a6875879576
Felipe Amador <felipedavidamador@gmail.com>
2020-09-30 16:47:13 -0300

    feat: add kits models

--- kits/models.py
+++ kits/models.py
@@ -1,3 +1,23 @@
-from django.db import models
+"""Contains the kits app models."""
+import uuid
 
-# Create your models here.
+from djongo import models
+
+from products.models import Product
+
+
+class Kit(models.Model):
+    """Define the kits model."""
+    id = models.UUIDField(primary_key=True, unique=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    sku = models.CharField(max_length=100, unique=True)
+    cost = models.FloatField()
+    price = models.FloatField()
+    stock = models.IntegerField()
+    products = models.ManyToManyField(to=Product)
+
+    def __str__(self) -> str:
+        """
+        Return the Kit instance string representation.
+        """
+        return f"{id} - name - sku - price"
